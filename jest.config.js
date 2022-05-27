62f72e42ddb49d0e521c70dcbcd127ab221408fa
Nicoli Ferreira <nicoli.ferreira@unifesp.br>
2022-05-27 10:36:25 -0300

    chore: add jest env and eslint config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-
-  // A list of paths to directories that Jest should use to search for files in
-  roots: ['<rootDir/src>'],
-
-  // The directory where Jest should output its coverage files
+  roots: ['.'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
-  testEnvironment: 'Node',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts>'],
+  testEnvironment: 'node',
   transform: {
-    '+\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
   }
 }
